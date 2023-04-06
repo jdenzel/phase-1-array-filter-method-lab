60b2ac76388d70964fb19ec94b0e68e6babc22ce
Denzel <jdenzelmab@gmail.com>
2023-04-06 16:41:43 -0400

    Completed array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(list, name){
+    let driver = list.filter((num) => num == name || num == name.toLowerCase());
+    return driver;
+} 
+function fuzzyMatch(list, letters){
+    let driver = list.filter((name) => name.startsWith(letters));
+    return driver;
+}
+function matchName(list, string){
+    let driver = list.filter((obj) => obj.name === string);
+    return driver;
+}
